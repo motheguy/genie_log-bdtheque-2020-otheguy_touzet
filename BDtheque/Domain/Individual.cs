653a7a15bb26a3de9811fc089eeffc81562be737
motheguy <motheguy@ensc.fr>
2020-12-10 18:55:47 +0100

    Initialisation

---
+++ BDtheque/Domain/Individual.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain
+{
+    ///<summary>
+    /// User and Admin's class parent  
+    ///</summary>
+    public class Individual
+    {
+        public virtual int Id { get; set; }
+        public virtual string Login { get; set; }
+        public virtual string Password { get; set; }
+        public Individual() { }
+    }
+}
---
+++ BDtheque/Domain/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class User : Individual
+    {
+        public virtual List<Album> ComicsOwned {get;set;}
+        public virtual List<Album> ComicsWanted { get; set; }
+
+        public User() { }
+        public void AddComicOwned(Album newAlbum)
+        {
+            ComicsOwned.Add(newAlbum);
+        }
+        public void AddComicWanted(Album newAlbum)
+        {
+            ComicsWanted.Add(newAlbum);
+        }
+    }
+}
