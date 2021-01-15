799dc91e4fb58732dffa252dbd8d16d7b66e6e14
Maika Touzet <maika.touzet@yahoo.fr>
2021-01-15 13:49:11 +0100

    remplissage BDD

--- BDtheque/DAL/DB/Content.sql
+++ BDtheque/DAL/DB/Content.sql
@@ -12,14 +12,22 @@ insert into album values (6, '../../../img/6.jpg', "Death Note - Tome 1", 'Death
 insert into album values (7, '../../../img/7.jpg', "Death Note - Tome 2", 'Death Note', 'Tsugumi Ōba, Takeshi Obata', 'Manga','Fantastique','Shūeisha');
 insert into album values (8, '../../../img/8.jpg', "Les v'la !", 'Boule & Bill', 'Jean Roba', 'BD','Enfants','Dargaud');
 insert into album values (9, '../../../img/9.jpg', "Quel cirque !", 'Boule & Bill', 'Laurent Verron', 'BD','Enfants','Dargaud');
-insert into album values (10, '../../../img/10.jpg', "On a marché sur la Lune", 'Tintin', 'Hergé', 'BD','aventure','Casterman');
-insert into album values (11, '../../../img/11.jpg', "On a marché sur la Lune", 'Tintin', 'Hergé', 'BD','aventure','Casterman');
-insert into album values (12, '../../../img/12.jpg', "On a marché sur la Lune", 'Tintin', 'Hergé', 'BD','aventure','Casterman');
-insert into album values (13, '../../../img/13.jpg', "On a marché sur la Lune", 'Tintin', 'Hergé', 'BD','aventure','Casterman');
-insert into album values (14, '../../../img/14.jpg', "On a marché sur la Lune", 'Tintin', 'Hergé', 'BD','aventure','Casterman');
-insert into album values (15, '../../../img/15.jpg', "On a marché sur la Lune", 'Tintin', 'Hergé', 'BD','aventure','Casterman');
+insert into album values (10, '../../../img/10.jpg', "Persepolis - Tome 1", 'Persepolis', 'Marjane Satrapi', 'BD','Autobiographie',"L'Association");
+insert into album values (11, '../../../img/11.jpg', "Persepolis - Tome 2", 'Persepolis', 'Marjane Satrapi', 'BD','Autobiographie',"L'Association");
+insert into album values (12, '../../../img/12.jpg', "Cap in His Own Magazine at Last!", 'Captain America', 'Stan Lee, Jack Kirby', 'Comic','Aventure','Marvel');
+insert into album values (13, '../../../img/13.jpg', "When wakes the sleeper!", 'Captain America', 'Stan Lee, Jack Kirby', 'Comic','Aventure','Marvel');
+insert into album values (14, '../../../img/14.jpg', "The Amazing Spider Man", 'Spider-Man', 'Humberto Ramos, Ramon Perez, Dan Slott', 'Comic','Aventure','Marvel');
+insert into album values (15, '../../../img/15.jpg', "Polina", '', 'Bastien Vivès', 'Roman Graphique','Portrait','Casterman');
 
 insert into bdsouhait values (1, 2, 1);
 insert into bdsouhait values (2, 2, 3);
+insert into bdsouhait values (3, 4, 1);
+insert into bdsouhait values (4, 4, 11);
+insert into bdsouhait values (5, 4, 15);
 
-insert into bdpossession values (1, 2, 4);
+insert into bdpossession values (1, 2, 2);
+insert into bdpossession values (2, 2, 4);
+insert into bdpossession values (3, 2, 11);
+insert into bdpossession values (1, 4, 4);
+insert into bdpossession values (4, 4, 10);
+
